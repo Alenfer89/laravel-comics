c3dcfb5ad6c962403a6bd63ceed5c5503be8c008
Alenfer89 <alberioalex@gmail.com>
2022-05-04 16:30:45 +0200

    cards-added-with-style

--- resources/views/users/guests/comics.blade.php
+++ resources/views/users/guests/comics.blade.php
@@ -3,5 +3,27 @@
 @section('title', 'Our Comics')
 
 @section('main-content')
-    
+<div id="index-main">
+    <div id="product-background">
+        <div class="my-container">
+            <span>current series</span>
+            <div class="product-container">
+                @foreach ($comics as $comic)
+                    <div class="comic-card">
+                        <div class="img-thumb-container">
+                            <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                        </div>
+                        <h5>{{ $comic['title'] }}</h5>
+                    </div>
+                @endforeach
+            </div>
+            <button>Load More</button>
+        </div>
+    </div>
+    <div id="products-links">
+        <div class="my-container">
+            {{-- <NavLinks /> --}}
+        </div>
+    </div>
+</div>
 @endsection
