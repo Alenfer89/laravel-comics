8623e69a98c5309eb72c6c6c786c73b0ca67085e
Alenfer89 <alberioalex@gmail.com>
2022-05-05 17:12:07 +0200

    added-more-comic-details-with-poor-css

--- resources/views/users/guests/comic-detail.blade.php
+++ resources/views/users/guests/comic-detail.blade.php
@@ -30,9 +30,60 @@
     
     <section class="bot-comic">
         <div class="ax-comic-container">
-    
+            <div class="ax-row">
+                <div class="ax-col-5">
+                    <h3>Talent</h3>
+                    <p></p>
+                    <p>Art By:
+                        <span>
+                            @foreach ($comic['artists'] as $index => $artist)
+                                @if($index !== array_key_last($comic['artists']))
+                                    <?php echo $artist . ', '; ?> 
+                                @elseif($index == array_key_last($comic['artists']))
+                                <?php echo $artist . '.'; ?>
+                                @endif
+                            @endforeach
+                        </span>
+                    </p>
+                    <p>Written By:
+                        <span>
+                            @foreach ($comic['writers'] as $index => $artist)
+                                @if($index !== array_key_last($comic['writers']))
+                                    <?php echo $artist . ', '; ?> 
+                                @elseif($index == array_key_last($comic['writers']))
+                                <?php echo $artist . '.'; ?>
+                                @endif
+                            @endforeach
+                        </span>
+                        
+                    </p>
+                </div>
+                <div class="ax-col-5">
+                    <h3>Specs</h3>
+                    <p></p>
+                    <p>
+                        Series:
+                        <span>
+                            {{ $comic['series'] }}
+                        </span>
+                    </p>
+                    <p>
+                        US Price:
+                        <span>
+                            {{ $comic['price'] }}
+                        </span>
+                    </p>
+                    <p>
+                        On Sale date:
+                        <span>
+                            {{ $comic['sale_date'] }}
+                        </span>
+                    </p>
+                </div>
+            </div>
+            
         </div>
     </section>
 </div>
-@dump($comic)
+@dump($comic['artists'])
 @endsection
