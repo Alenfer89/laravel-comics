3e356e31c3bcef6312767c6f9cee710129ac2a9c
Alenfer89 <alberioalex@gmail.com>
2022-05-05 15:46:18 +0200

    detail-connection

---
+++ resources/views/users/guests/comic-detail.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('title', '- Our Comics')
+
+@section('main-content')
+<section class="top-comic-header">
+    <div class="ax-comic-container">
+
+    </div>
+</section>
+<section>
+    
+</section>
+<section>
+    
+</section>
+@dump($comic)
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Hamcrest\Type\IsNumeric;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,9 +22,18 @@ Route::get('/home', function () {
     return view('users.guests.home');
 })->name('home');
 
-
 Route::get('/comics', function () {
     $comics = config('comics');
     $contentLinks = config('products-links');
     return view('users.guests.comics', ['comics' => $comics, 'contentLinks' => $contentLinks]);
 })->name('comics');
+
+Route::get('/comics/{index}', function ($index) {
+    $comics = config('comics');
+    $contentLinks = config('products-links');
+    if( is_numeric($index) && $index >= 0 && $index < count($comics)){
+    return view('users.guests.comic-detail', ['comic' => $comics[$index], 'contentLinks' => $contentLinks]);
+    } else {
+        abort(404);
+    }
+})->name('comic');
