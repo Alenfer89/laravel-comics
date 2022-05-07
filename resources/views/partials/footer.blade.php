1ca43a179042bad2cc03796cfe8cf0668964714a
Alenfer89 <alberioalex@gmail.com>
2022-05-07 09:54:50 +0200

    not so lovely implemnt of footer links

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
 
     <div class="top-footer-container">
         <div class="top-footer my-container">
-            <div class="lists-container">
+            {{-- <div class="lists-container">
                 @foreach ($links as $key => $link)
                     @if ($key !== array_key_last($links))
                         <div class="list-wrapper">
@@ -19,6 +19,56 @@
                         </div>
                     @endif
                 @endforeach
+            </div> --}}
+            <div class="lists-container">
+                <div class="list-wrapper">
+                    @foreach ($links as $key => $link)
+                        @if ($key === 'shop' || $key === 'dc Comics')
+                            <h4> {{ $key }}</h4>
+                            <ul>
+                                @foreach ($link as $linkSpecific)
+                                    <li>
+                                        <a href="{{ $linkSpecific['url'] }}">
+                                            {{ $linkSpecific['text'] }}
+                                        </a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        @endif
+                    @endforeach
+                </div>
+                <div class="list-wrapper">
+                    @foreach ($links as $key => $link)
+                        @if ($key === 'dc')
+                            <h4> {{ $key }}</h4>
+                            <ul>
+                                @foreach ($link as $linkSpecific)
+                                    <li>
+                                        <a href="{{ $linkSpecific['url'] }}">
+                                            {{ $linkSpecific['text'] }}
+                                        </a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        @endif
+                    @endforeach
+                </div>
+                <div class="list-wrapper">
+                    @foreach ($links as $key => $link)
+                        @if ($key === 'sites')
+                            <h4> {{ $key }}</h4>
+                            <ul>
+                                @foreach ($link as $linkSpecific)
+                                    <li>
+                                        <a href="{{ $linkSpecific['url'] }}">
+                                            {{ $linkSpecific['text'] }}
+                                        </a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        @endif
+                    @endforeach
+                </div>
             </div>
             <div id="logo-background">
             </div>
